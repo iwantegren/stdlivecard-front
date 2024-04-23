d2d496706ec3a7b1a069754aa3fb555a1bd8f6f5
Ivan Bilan <ib@decard.io>
2024-04-23 21:28:06 +0300

    receive user by id

--- src/components/DataComponent.tsx
+++ src/components/DataComponent.tsx
@@ -1,21 +1,50 @@
-import { fetchData } from "../services/ApiService";
+import * as api from "../services/ApiService";
 import { useState, useEffect } from "react";
+
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const DataComponent = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const loadData = async () => {
+    setLoading(true);
+    await sleep(1000);
+    api
+      .getUser("lovelyUser")
+      .then((response) => {
+        setData(response);
+        setError(null);
+      })
+      .catch((err) => {
+        setData(null);
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  };
 
   useEffect(() => {
-    fetchData().then(setData).catch(setError);
-  }, []); // Empty dependency array means this effect runs once on mount
+    loadData();
+  }, []);
 
   if (error) return <div>Error loading data</div>;
-  if (!data) return <div>Loading...</div>;
+  if (loading) return <div>Loading...</div>;
 
   return (
     <div>
       <h1>Server is alive</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <button
+        type="button"
+        className="btn btn-info"
+        disabled={loading}
+        onClick={loadData}
+      >
+        Update Status
+      </button>
     </div>
   );
 };
--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,8 +1,21 @@
 const BASE_URL = "http://localhost:3000";
 
-export const fetchData = async () => {
+export const getStatus = async () => {
   try {
     const response = await fetch(`${BASE_URL}/`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+};
+
+export const getUser = async (id: string) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${id}`);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
